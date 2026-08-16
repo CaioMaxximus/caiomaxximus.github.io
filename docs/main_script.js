3552a7c2be3b2729a11210e514ab8b2c35a5f4ed
CaioMaxximus <caio.soares@ccc.ufcg.edu.br>
2026-08-15 22:36:21 -0300

    Adding the  proper links

--- docs/main_script.js
+++ docs/main_script.js
@@ -14,7 +14,7 @@ function Certification(title, image, link){
 
 
 const software_projects = [
-    new Project("Onion PY",
+    new Project("Onion PY - An anonymus tor P2P message  service build with python",
         `
             Writing a anonymus message chat to host and connect over the Tor network in python using a graphic static library as interface
 
@@ -22,13 +22,13 @@ const software_projects = [
                 - Use asynchronus techiniques to isolate responsabilities from each application layer
                 - Asynchonus queues and event bus allow a fluid operation and strong arquitetural componetization 
         `,
-        "assets/software_projects/logo_onio_py.png", "" ,["Python" ,"Tor" , "TCP/Server" ,"P2P"]
+        "assets/software_projects/logo_onio_py.png", "https://github.com/CaioMaxximus/onionpy-visual-chat" ,["Python" ,"Tor" , "TCP/Server" ,"P2P"]
     ),
     new Project("Quote Frame ",
         `
             A web application that combines FastAPI with NLP to return a random quote
              and a thematically associated photograph with each iteration.
-        `
+        ` ,  "assets/software_projects/quote_frame_img.png" ,"https://github.com/CaioMaxximus/Quote_Frame" , ["Python" , "JavaScript" , "FastAPI" , "React"]
     )
     
 
@@ -37,14 +37,14 @@ const software_projects = [
 const data_science_projects = [
     new Project("Exploring different in Linear Regression with input transformation",
         "This is a practical study on how Linear Regression can be used to approximate different types of curves through input transformation, allowing for a simple and quick solution that can be used to model different types of more complex curves.",
-        "assets/software_projects/linear_regression.jpeg",""
+        "assets/software_projects/linear_regression.jpeg","https://github.com/CaioMaxximus/Exploring-Different-Curves-with-Linear-Regression"
     ),
     new Project("Coffee Shop - Sales analysis", 
         "This new Project analyzes sales data from a coffee shop located in Vinnytsia, Ukraine, with the goal of understanding demand patterns and identifying key drivers of sales variability over time",
-    "assets/data_science_imgs/coffe_sales_proj.png", ""),
+    "assets/data_science_imgs/coffe_sales_proj.png", "https://github.com/CaioMaxximus/CoffeeShop_Analysis"),
     new Project("Time Series Decomposition", 
         "This notebook explore classic time series decomposition techiniques to understand sales patterns in a bike store.",
-    "assets/data_science_imgs/bike_sales_proj.png", "")
+    "assets/data_science_imgs/bike_sales_proj.png", "https://github.com/CaioMaxximus/Time_Series_Decomposition-bike_sales_america")
 ];
 
 const certifications = [
